f839e9ddb74dc8b627d629cc875b0e5f3e2a0be8
unknown <truedmaker@gmail.com>
2021-11-18 05:53:49 -0300

    first commit

---
+++ assets/php/cadastro.php
@@ -0,0 +1,19 @@
+<?php
+
+    $name=$_GET["name"];
+    $rg=$_GET["RG"];
+    $cpf=$_GET["CPF"];
+    $adress=$_GET["adress"];
+    $genere=$_GET["genere"];
+    $birthday=$_GET["birthday"];
+    $maritalStatus=$_GET["check"];
+
+    echo $name;
+    echo "<br/>";
+    echo $rg;
+    echo $cpf;
+    echo $adress;
+    echo $genere;
+    echo $birthday;
+    echo $maritalStatus;
+?>
---
+++ assets/scripts/eventHandler.js
@@ -0,0 +1,19 @@
+const produtos = document.getElementById('produtos');
+const index = document.querySelectorAll('img.index-products');
+
+const jordan = document.querySelector('div.sub-hero');
+
+produtos.addEventListener('mouseover', () => {
+    document.querySelector('div.produtos-hover').style.display = "grid";
+    for(let i = 0; i < 3; i++){
+        index[i].style.filter = "blur(5px)";
+    }
+});
+
+produtos.addEventListener('mouseout', () => {
+    document.querySelector('div.produtos-hover').style.display = "none";
+    for(let i = 0; i < 3; i++){
+        index[i].style.filter = "blur(0px)";
+    }
+});
+
